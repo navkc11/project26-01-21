6f6cf762303d1be743a8483dee24484d38f4f621
navkc11 <63931144+navkc11@users.noreply.github.com>
2021-01-28 04:47:24 +0200

    Update Company.java

--- Company.java
+++ Company.java
@@ -54,29 +54,35 @@ public class Company {
     void addWorker(Worker w) { allWorkers.add(w); }
 
     //method 5
-    //לא הצלחתי להשתמש בComparator
-//    void crisis() {
-//        ArrayList<Worker> highestPayed= allWorkers;
-//        ArrayList<Worker> mostOvertime= allWorkers;
-//        Comparator<Worker> compareByPay = new Comparator<Worker>() {
-//            @Override
-//            public int compare(Worker o1, Worker o2) {
-//                return o1.getPay().compareTo(o2.getPay());
-//            }
-//        };
-//
-//
-//
-//
-//
-//        Collections.sort(highestPayed);
-//        for (int i = 0;i<allWorkers.size();i++){
-//            if (highestPayed.size()<= allWorkers.size()/4){
-//                highestPayed.add(allWorkers.get(i));
-//                else{
-//
-//                }
-//            }
-//        }
-//    }
+    void crisis() {
+        ArrayList<Worker> mostOvertime = new ArrayList<>();
+        ArrayList<Worker> highestPayed = new ArrayList<>();
+        //sorting workers by most overtime
+        Collections.sort(allWorkers, new Comparator<Worker>() {
+            @Override
+            public int compare(Worker o1, Worker o2) {return o1.overtimeHours().compareTo(o2.overtimeHours());}
+        });
+        //adding the highest 25% to mostOvertime
+        for (int i = allWorkers.size()-1;i>= (allWorkers.size()*0.75);i--) {
+            mostOvertime.add(allWorkers.get(i));
+        }
+        //sorting workers by highest pay
+        Collections.sort(allWorkers,((o1, o2) -> o1.getPay().compareTo(o2.getPay())));
+        //adding the highest 25% to highestPayed
+        for (int i = allWorkers.size()-1;i>= allWorkers.size()*0.75;i--) {
+            highestPayed.add(allWorkers.get(i));
+        }
+        //remove most overtime
+        for (Worker hoursWorker:mostOvertime){
+            if(allWorkers.contains(hoursWorker)){
+                allWorkers.remove(hoursWorker);
+            }
+        }
+        //remove highest payed
+        for (Worker payedWorker : highestPayed){
+            if(allWorkers.contains(payedWorker)){
+                allWorkers.remove(payedWorker);
+            }
+        }
+    }
 }
